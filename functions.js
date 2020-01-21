e832751699b0fd9cb69e33abb69c5f3e5885cf80
John Fleming <johndeclanfleming@gmail.com>
2020-01-20 19:09:56 -0600

    finished function challenges

--- functions.js
+++ functions.js
@@ -19,3 +19,22 @@ const dogBreed = (dog) => {
 
 console.log(dogBreed('lab'));
 console.log(dogBreed('actually a cat'));
+
+// challenge 1 - write a function that adds 42 to an inputted number
+const fortyTwo = (number) => {
+    const output = number + 42;
+    return output;
+};
+
+console.log(fortyTwo(50));
+console.log(fortyTwo(100));
+
+// challenge 2 - write a function that takes your birth year as an input and tells you how old you will be in 2099
+const oldAge = (birthYear) => {
+    const age = 2099 - birthYear;
+    const output = `You will be ${age} in 2099`;
+    return output;
+};
+
+console.log(oldAge(1991));
+console.log(oldAge(2000));
